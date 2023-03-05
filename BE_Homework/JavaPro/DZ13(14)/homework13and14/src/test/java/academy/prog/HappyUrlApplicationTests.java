2cfe525eab0ea345687b981121d9bb568aec169b
OlTat <115583269+OlTat@users.noreply.github.com>
2023-03-05 12:36:24 +0200

    Add files via upload

---
+++ BE_Homework/JavaPro/DZ13(14)/homework13and14/src/test/java/academy/prog/HappyUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.prog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
